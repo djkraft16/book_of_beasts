efc084397806f90a02c1476509f256dd9c5affe7
kraf9284 <kraf9284@stthomas.edu>
2024-06-06 19:36:25 -0500

    Initial Commit: Setup

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hee Hee Hee Hah
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, (error) => {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  });
+}
+
 root.render(
   <React.StrictMode>
     <App />
